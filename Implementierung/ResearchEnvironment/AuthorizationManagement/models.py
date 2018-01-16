c59473f83c3c94dd53e63d52cdfee92e02386b34
Sashks <alex.butschie@gmail.com>
2018-01-16 23:42:42 +0100

    readers and owners many2many connections

--- Implementierung/ResearchEnvironment/AuthorizationManagement/models.py
+++ Implementierung/ResearchEnvironment/AuthorizationManagement/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class CustomUser(User):
@@ -20,6 +21,8 @@ class CustomUser(User):
         
 class Owner(CustomUser):
     
+    class Meta:
+        proxy = True
     def giveAccessPermission(self, Resource, CustomUser):
         pass
     def allowAccessPermission(self, Request):
@@ -36,6 +39,8 @@ class Owner(CustomUser):
     
 class Admin(Owner):
     
+    class Meta:
+        proxy = True    
     def searchUser(self,CustomUser):
         pass
     def blockUser(self,CustomUser):
@@ -56,7 +61,8 @@ class Resource(models.Model):
     name = models.CharField(max_length=150, default = 'default_name')
     description = models.CharField(max_length=250, default = 'default_description')
     creationDate = models.DateTimeField
-    readers = models.ManyToManyField(CustomUser)
+    readers = models.ManyToManyField(CustomUser, related_name='readers')
+    owners = models.ManyToManyField(Owner, related_name='owners')
     
     def hasAccessPermission(self,CustomUser):
         pass
