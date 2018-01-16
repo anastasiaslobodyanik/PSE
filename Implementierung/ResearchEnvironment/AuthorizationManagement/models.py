abaaabab299dc8063c08a118ff120071e3d8343d
SonyaV <33463478+SonyaV@users.noreply.github.com>
2018-01-16 11:24:52 +0100

    added method signatures
    ignore the other 'changed' files, smth went wrong

--- Implementierung/ResearchEnvironment/AuthorizationManagement/models.py
+++ Implementierung/ResearchEnvironment/AuthorizationManagement/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.migrations import 0008_alter_user_username_max_length
 
 # Create your models here.
 
@@ -8,24 +7,61 @@ class CustomUser(User):
     
     class Meta:
         proxy = True
+    def searchResources(self, Resource):
+        pass
+    def addResource(self):
+        pass
+    def accessResource(self,Resource):
+        pass
+    def sendAccessRequest(self, Resource):
+        pass
+    def cancelRequest(self, Request):
+        pass
         
 class Owner(CustomUser):
     
-    def test(self):
-        print("I am an owner.")
+    def giveAccessPermission(self, Resource, CustomUser):
+        pass
+    def allowAccessPermission(self, Request):
+        pass
+    def deleteAccessPermission(self, Resource, CustomUser):
+        pass
+    def denyAccessPermission(self,Request):
+        pass
+    def allowOwnerPermission(self,Resource,CustomUser):
+        pass
+    def sendDeletionRequest(self,Resource):
+        pass
+    
     
 class Admin(Owner):
     
-    def test(self):
-        Owner.test(self)
-        print("I am also an admin.")
+    def searchUser(self,CustomUser):
+        pass
+    def blockUser(self,CustomUser):
+        pass
+    def deleteUser(self,CustomUser):
+        pass
+    def deleteResource(self,Resource):
+        pass
+    def acceptDeletionRequest(self,Request):
+        pass
+    def denyDeletionRequest(self,Request):
+        pass
+    def deleteOwnerPermission(self,Resource,CustomUser):
+        pass
     
 class Resource(models.Model):
-    type = models.CharField
-    name = models.CharField
-    description = models.CharField
+    type = models.CharField(max_length=50, default = 'default_type')
+    name = models.CharField(max_length=150, default = 'default_name')
+    description = models.CharField(max_length=250, default = 'default_description')
     creationDate = models.DateTimeField
     readers = models.ManyToManyField(CustomUser)
+    
+    def hasAccessPermission(self,CustomUser):
+        pass
+    def hasOwnerPermission(self,CustomUser):
+        pass
     
 class Request(models.Model):
     sender = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
@@ -34,7 +70,12 @@ class Request(models.Model):
     
     class Meta:
         abstract = True
-        
+    
+    def deny(self):
+        pass
+    def accept(self):
+        pass
+    
 class AccessRequest(Request):
     
     def accessR(self):
