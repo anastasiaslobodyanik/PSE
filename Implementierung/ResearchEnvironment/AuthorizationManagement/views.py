efd00dade1705190829c72596e2782e1d3cf27bd
DannieSakly <Saklykhalil@hotmail.fr>
2018-01-20 14:08:26 +0100

    added login_required for home view

--- Implementierung/ResearchEnvironment/AuthorizationManagement/views.py
+++ Implementierung/ResearchEnvironment/AuthorizationManagement/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     return HttpResponse("Test home page rendering using def. Later on it must be rewritten with view class.")
@@ -12,6 +14,7 @@ def foo(request, requestID=None, resourceID=None, userID=None):
     else:
         html = "<html><body>Test!resourceID = %s .</body></html>" % resourceID 
     return HttpResponse(html)
+@login_required(login_url='dummy/login')
 def homeView(request):
     return render(request, 'homeView.html')
 def profileView(request):
